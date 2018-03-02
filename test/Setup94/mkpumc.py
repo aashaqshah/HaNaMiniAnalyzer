8d871a880c0e65f9b235ee9b9687ff1919831bab
aashaq shah <aashiqphst@gmail.com>
2018-03-02 12:32:51 +0100

    ready for preliminary run

--- test/Setup94/mkpumc.py
+++ test/Setup94/mkpumc.py
@@ -1,6 +1,7 @@
 #/cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw-patch/CMSSW_8_0_8_patch1/src/SimGeneral/MixingModule/python/
 #/cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw-patch/CMSSW_8_0_8_patch1/src/SimGeneral/MixingModule/python/mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU_cfi.py
 
+
 #probFunctionVariable = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49)
 probValue = (
     0.000829312873542,
